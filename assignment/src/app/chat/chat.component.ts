64b182b961f7ecd43ed3689016b7e787d2ee92e0
Samuel Wren <samwren7@hotmail.com>
2018-09-03 11:30:21 +1000

    edited chat ts

--- assignment/src/app/chat/chat.component.ts
+++ assignment/src/app/chat/chat.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { SocketService } from '../socket.service';
+import { Router } from '@angular/router';
+import{ FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-chat',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
 
-  constructor() { }
+	username: string;
+	message: string;
+	messages: string[] = [];
+	connection;
 
-  ngOnInit() {
-  }
+	constructor(private socketService: SocketService, private router:Router, private forms:FormsModule) { }
+
+	sendMessage( event ) {
+		event.preventDefault();
+	  	this.socketService.sendMessage(this.message + ' (' + this.username + ')');
+	  	console.log(this.message);
+	}
+
+	ngOnInit() {
+		this.username = localStorage.getItem("username");
+
+	    this.connection = this.socketService
+	      .getMessages()
+	      .subscribe((message: string) => {
+	        this.messages.push(message);
+	        this.message = '';
+	      });
+	}
 
 }
