e85a67f94ac7999c0e09eb1938f0bc1ce074b3f0
Samuel Wren <samwren7@hotmail.com>
2018-09-03 13:48:54 +1000

    editted socket files

--- assignment/src/app/socket.service.ts
+++ assignment/src/app/socket.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import * as io from 'socket.io-client';
+import { Observable } from 'rxjs';
+import { Http, Response } from '@angular/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SocketService {
 
-  constructor() { }
+	private url = 'http://localhost:3000';
+    private socket;    
+
+    constructor(private http: Http) {
+    }
+
+    public sendMessage(message) {
+        this.socket.emit('new-message', message);
+    }
+
+    public getMessages = () => {
+        return Observable.create((observer) => {
+            this.socket = io();
+            this.socket.on('message', (data) => {
+                observer.next(data);
+            });
+        });
+    }
 }
