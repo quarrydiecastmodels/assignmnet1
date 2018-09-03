26f05458a3b8c46f79aa8ef973ec8f94f0934dd2
Samuel Wren <samwren7@hotmail.com>
2018-09-03 13:38:35 +1000

    added to module.ts and fixed login.html

--- assignment/src/app/app.module.ts
+++ assignment/src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { SocketService } from './socket.service';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -12,9 +16,18 @@ import { ChatComponent } from './chat/chat.component';
     ChatComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot([       
+        {"path":"chat", "component":ChatComponent},
+        {"path":"login", "component":LoginComponent},
+        {"path":"users", "component":UsersComponent},
+        {"path":"dashboard", "component":DashboardComponent},
+    ]),
   ],
-  providers: [],
+  ],
+  providers: [SocketService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
